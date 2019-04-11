21708f4e7ceff2a41a50eaf049bd669877d665e5
DEO <f.db@163.com>
2019-04-11 21:44:51 +0800

    初始化项目

---
+++ SyncVideo/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace SyncVideo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.axPlayer1 = new AxAPlayer3Lib.AxPlayer();
+            ((System.ComponentModel.ISupportInitialize)(this.axPlayer1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // axPlayer1
+            // 
+            this.axPlayer1.Enabled = true;
+            this.axPlayer1.Location = new System.Drawing.Point(-3, 2);
+            this.axPlayer1.Name = "axPlayer1";
+            this.axPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axPlayer1.OcxState")));
+            this.axPlayer1.Size = new System.Drawing.Size(837, 487);
+            this.axPlayer1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(18F, 36F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.axPlayer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.axPlayer1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private AxAPlayer3Lib.AxPlayer axPlayer1;
+    }
+}
